a59c5ef07f5c076bf36dafcb3b7b8805f73765b4
Marco Bulgarelli - marco.bulgarelli6@studio.unibo.it <marco.bulgarelli6@studio.unibo.it>
2024-11-25 12:17:24 +0100

    Finished exercise 02

--- 02-fill-class-student/Student.java
+++ 02-fill-class-student/Student.java
@@ -1,16 +1,29 @@
 class Student {
 
     // È buona pratica mettere i campi in testa alla classe
+    String name;
+    String surname;
+    int id;
+    int matriculationYear;
 
-    void build(/* Qui vanno i parametri necessari a inizializzare l'oggetto */) {
+    void build(String n, String s, int id, int mY) {
         /*
          * Completare il corpo del metodo
          */
+            this.name = n;
+            this.surname = s;
+            this.id = id;
+            this.matriculationYear = mY;
     }
 
     void printStudentInfo() {
         /*
          * Completare il corpo del metodo
          */
+        System.out.println("Student:");
+        System.out.println("Name: " + this.name);
+        System.out.println("Surname: " + this.surname);
+        System.out.println("ID: " + this.id);
+        System.out.println("Matriculation year: " + this.matriculationYear);
     }
 }
--- 02-fill-class-student/TestStudent.java
+++ 02-fill-class-student/TestStudent.java
@@ -1,8 +1,14 @@
 class TestStudent {
 
     public static void main(String[] args) {
-        Student someone = new Student();
-        someone.build();
-        someone.printStudentInfo();
+        Student student1 = new Student();
+        Student student2 = new Student();
+        Student student3 = new Student();
+        student1.build("Alex", "Balducci", 1015, 2019);
+        student2.build("Angel", "Bianchi", 1016, 2018);
+        student3.build("Andrea", "Bracci", 1017, 2017);
+        student1.printStudentInfo();
+        student2.printStudentInfo();
+        student3.printStudentInfo();
     }
 }
