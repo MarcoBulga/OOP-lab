d28eef1cb3f0209afaf014bbbf5fdee8821a92c1
Marco Bulgarelli - marco.bulgarelli6@studio.unibo.it <marco.bulgarelli6@studio.unibo.it>
2024-11-25 12:48:33 +0100

    Finished exercise 03

--- 03-impl-complexnum/TestComplexNum.java
+++ 03-impl-complexnum/TestComplexNum.java
@@ -30,5 +30,46 @@ class TestComplexNum {
        *
        * 11) Controllare se l'output corrisponde a quanto preventivato
        */
+
+      //1) Creo il numero complesso 3+5i e lo memorizzo in c1
+      ComplexNum c1 = new ComplexNum();
+      c1.build(3, 5);
+
+      //2) Creo il numero complesso 7-4i e lo memorizzo in c2
+      ComplexNum c2 = new ComplexNum();
+      c2.build(7, -4);
+
+      //3) Creo il numero complesso -2+3i e lo memorizzo in c2
+      ComplexNum c3 = new ComplexNum();
+      c3.build(-2, 3);
+      
+      //4) Creo il numero complesso -2+3i e lo memorizzo in c4
+      ComplexNum c4 = new ComplexNum();
+      c4.build(-2, 3);
+
+      //5) Stampo la rappresentazione testuale di c1, c2, c3 e c4
+      System.out.println(c1.toStringRep());
+      System.out.println(c2.toStringRep());
+      System.out.println(c3.toStringRep());
+      System.out.println(c4.toStringRep());
+      System.out.println("");
+
+      //6) Sommo a c1 c2
+      c1.add(c2);
+
+      //7) Sommo a c2 c4
+      c2.add(c4);
+
+      //8) Stampo di nuovo tutti i numeri
+      System.out.println(c1.toStringRep());
+      System.out.println(c2.toStringRep());
+      System.out.println(c3.toStringRep());
+      System.out.println(c4.toStringRep());
+      System.out.println("");
+
+      //10) Invoco il metodo equals su c3 passando a parametro tutti gli altri numeri e stampo a schermo il risultato
+      System.out.println("c3 is equal to c1?   " + c3.equal(c1));
+      System.out.println("c3 is equal to c2?   " + c3.equal(c2));
+      System.out.println("c3 is equal to c4?   " + c3.equal(c4));
     }
 }
